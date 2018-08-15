eb39d31b093188c3f4df670efbdd330df1c0c9e5
techknowsurfer <shindesaagar@gmail.com>
2018-08-15 10:05:34 +0530

    Fixing the yml properties file.

--- src/main/resources/data/post-h2.sql
+++ src/main/resources/data/post-h2.sql
@@ -1,2 +1,9 @@
 INSERT INTO POST (POST_ID,CONTENT,ARTICLE_TITLE,STATUS,AUTHOR_ID) VALUES
     (1,'Article on host to create Spring Data Rest Resources','Getting Started with Spring Data Rest and HATEOAS',true,1);
+
+INSERT INTO POST (POST_ID,CONTENT,ARTICLE_TITLE,STATUS,AUTHOR_ID) VALUES
+    (2,'This article talk about basics of Getting started with Hibernate',
+    'Getting Started with Hibernate',true,2);
+
+INSERT INTO POST (POST_ID,CONTENT,ARTICLE_TITLE,STATUS,AUTHOR_ID) VALUES
+    (3,'This article talk about basics of Getting started with Java','Getting Started with Java',true,3);
